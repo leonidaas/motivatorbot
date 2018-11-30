2a6f81217e33b245e7c0302161a907c2b41b0757
= <Fuessner@jochen-schweizer.de>
2018-11-30 09:24:44 +0100

    added ability to scroll down the pages for more quotes

--- HappyBot.py
+++ HappyBot.py
@@ -9,6 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 crawler = Crawler()
+
 cid = int(454323424)
 
 print("HappyBot wurde instanziiert")
@@ -19,7 +20,8 @@ dispatcher = updater.dispatcher
 
 #answer section
 def start(bot, update):
-    print(update.message.chat_id)
+    crawler.cid = update.message.chat_id
+    job.run_repeating(callback_quote, interval=1800, first=0)
     bot.send_message(chat_id=update.message.chat_id, text="Hi, ich bin Leons persönlicher Bot. Ich helfe ihm dabei fokusiert und motiviert zu bleiben")
 
 def unknown(bot, update):
@@ -34,20 +36,30 @@ def quote(bot, update):
 
 def fetch(bot, update):
     crawler.fetchQuotes()
+    print(len(crawler.quotes))
     bot.send_message(chat_id=update.message.chat_id, text="Ich besorch mal eben neue Quotes!")
+
+def fetchMore(bot, update):
+    crawler.fetchMoreQuotes()
+    print(len(crawler.quotes))
+    bot.send_message(chat_id=update.message.chat_id, text="Ich scroll mal bissale nunder!")
 
 def callback_quote(bot, job):
     print("message sent")
     quote = crawler.getRandomQuote()
-    bot.send_message(chat_id=cid, text=quote)
+    crawler.fetchMoreQuotes()
+    bot.send_message(chat_id=crawler.cid, text=quote)
 
-
-job.run_repeating(callback_quote, interval=1800, first=0)
+def showQuotes(bot, update):
+    for q in crawler.quotes:
+        print(q)
 
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("caps", caps, pass_args=True))
 dispatcher.add_handler(CommandHandler("quote", quote))
 dispatcher.add_handler(CommandHandler("fetch", fetch))
+dispatcher.add_handler(CommandHandler("fetchMore", fetchMore))
+dispatcher.add_handler(CommandHandler("showQuotes", showQuotes))
 # this must be the last argument so it goes through all commands
 dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
--- quotes_crawler.py
+++ quotes_crawler.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 import random
+import json
 
 
 class Crawler:
@@ -11,13 +12,17 @@ class Crawler:
         self.quotes = list()
         self.source = requests.get(self.url)
         plain_text = self.source.text
+        self.cid = 0
+        self.counter = 1
 
         obj = BS(plain_text, "html5lib")
 
         for a in obj.find_all('a', {'title': 'view quote'}):
+            #print (a.text)
             self.quotes.append(
                 a.text
             )
+
 
     def getRandomQuote(self):
         quote = self.quotes[random.randint(0, len(self.quotes))]
@@ -39,3 +44,40 @@ class Crawler:
             self.quotes.append(
                 a.text
             )
+        self.counter = 1
+
+    def fetchMoreQuotes(self):
+        print("scrolling down the page")
+        self.api = "https://www.brainyquote.com/api/inf"
+
+        self.data = {}
+        self.data['typ'] = "topic"
+        self.data['langc'] = "en"
+        self.data['v'] = "8.5.4:3062111"
+        self.data['ab'] = "b"
+        self.data['pg'] = self.counter
+        self.data['id'] = "t:132622"
+        self.data['vid'] = "7b363d749b4c7c684ace871c8a75f8e6"
+        self.data['fdd'] = "d"
+        self.data['m'] = 0
+
+        self.source = requests.request(method="get", url=self.api, json=self.data)
+        print(self.source)
+        self.counter += 1
+        obj2 = BS(self.source.content, "html5lib")
+
+        stringToSlice = str(obj2)
+        finishedString = stringToSlice[50:len(obj2.text) - 16]
+
+        soup = BS(finishedString, "html5lib")
+
+        for a in soup.find_all('a', {'class', '\\"b-qt'}):
+            self.quotes.append(
+                a.text
+            )
+
+
+
+    def printAllQuotes(self):
+        for q in self.quotes:
+            print(q)
